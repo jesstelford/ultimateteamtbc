18ec3af70e285b3ec885891cd263b502bb5350c7
Jess Telford <jess@jt0.org>
2011-07-14 22:32:02 +1000

    Refactored to be cleaner code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -34,35 +34,65 @@ public class Application extends Controller {
         render(myName);
     }
 
-    public static void login(String cont, String openid_identifier) {
-
-        // return to this action once logged in
-        if (GAE.isLoggedIn()) {
-            if (cont != null) {
-                redirect(cont);
-            } else {
-                index();
-            }
-        }
-        
-        // if federated login provided, log them in
-        if (openid_identifier != null) {
-
-            HashMap<String, Object> returnParams = new HashMap<String, Object>();
-
-            if (cont != null) {
-                returnParams.put("cont", cont);
-            }
-
-            try {
-                GAE.login("Application.login", returnParams, openid_identifier);
-            } catch(IllegalArgumentException iae) {
-                flash.error(iae.toString());
-            }
-
-        }
-
+    // TO login we redirect them if they are already logged in. We redirect
+    // them if they are logging in via OpenId. Lastly, we render the login
+    // page.
+    public static void login(String continueTo, String openid_identifier) {
+        redirectLoggedInUser(continueTo);
+        loginViaOpenId(continueTo, openid_identifier);
         render();
+    }
+
+    private static void redirectLoggedInUser(String continueTo) {
+        if (GAE.isLoggedIn()) {
+            redirectToContinuation(continueTo);
+        }
+    }
+
+    private static void loginViaOpenId(String continueTo, String openId) {
+        if (isValidOpenId(openId)) {
+            redirectToOpenIdLogin(continueTo, openId);
+        }
+    }
+
+    private static void redirectToContinuation(String continueTo) {
+        if (isValidContinuation(continueTo)) {
+            redirect(continueTo);
+        }
+        index();
+    }
+
+    private static boolean isValidOpenId(String openId) {
+        return (openId != null);
+    }
+
+    private static void redirectToOpenIdLogin(String continueTo, String openId) {
+
+        HashMap<String, Object> returnParams = generateOpenIdReturnParamaters(continueTo);
+        openIdLogin(returnParams, openId);
+    }
+
+    private static HashMap<String, Object> generateOpenIdReturnParamaters(String continueTo) {
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+
+        if (isValidContinuation(continueTo)) {
+            parameters.put("continueTo", continueTo);
+        }
+
+        return parameters;
+
+    }
+
+    private static void openIdLogin(HashMap<String, Object> paramaters, String openId) {
+        try {
+            GAE.login("Application.login", paramaters, openId);
+        } catch(IllegalArgumentException iae) {
+            flash.error(iae.toString());
+        }
+    }
+
+    private static boolean isValidContinuation(String continueTo) {
+        return (continueTo != null);
     }
 
     public static void logout() {
